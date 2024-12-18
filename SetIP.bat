b94a805a54dc09f0b3d8425e7d3c8b2657229413
Pengo <122631741+12637776@users.noreply.github.com>
2024-12-18 19:24:16 +0800

    Update SetIP.bat

--- SetIP.bat
+++ SetIP.bat
@@ -4,8 +4,16 @@ setlocal enabledelayedexpansion
 %~d0
 cd %~dp0
 set "csvFile=result.csv"
+warp-cli.exe tunnel endpoint reset
+cls
+set n=1
+echo 端点列表：
 for /f "skip=1 tokens=1 delims=," %%i in (%csvFile%) do (
-	warp-cli.exe tunnel endpoint reset
+        echo [!n!]%%i
+		set /a n+=1
+)
+echo 状态：
+for /f "skip=1 tokens=1 delims=," %%i in (%csvFile%) do (
 	echo 端点已设为 %%i
 	warp-cli.exe tunnel endpoint set %%i
 	echo 按任意键换下一个端点或者用Ctrl+C退出
