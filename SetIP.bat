d49ba8a8ab5e197bc82b7a5d1f2d480efe8743b7
Pengo <122631741+12637776@users.noreply.github.com>
2025-01-01 09:18:45 +0800

    Update SetIP.bat
    小升级：
    将当前的端口复制到粘贴板，以便在其他地方粘贴使用，譬如Wireguard。
    
    Copy the currently ip:port to the clipboard for pasting and use elsewhere, such as Wireguard.

--- SetIP.bat
+++ SetIP.bat
@@ -4,18 +4,22 @@ setlocal enabledelayedexpansion
 %~d0
 cd %~dp0
 set "csvFile=result.csv"
-warp-cli.exe tunnel endpoint reset
-cls
 set n=1
 echo 端点列表：
 for /f "skip=1 tokens=1 delims=," %%i in (%csvFile%) do (
-        echo [!n!]%%i
+        echo [!n!] %%i
 		set /a n+=1
 )
-echo 状态：
+echo.
+echo 端点状态：
 for /f "skip=1 tokens=1 delims=," %%i in (%csvFile%) do (
-	echo 端点已设为 %%i
+	echo ①端点重置
+	warp-cli.exe tunnel endpoint reset	
+	echo ②端点设为 %%i
 	warp-cli.exe tunnel endpoint set %%i
+	echo %%i>ipp.txt
+	type ipp.txt | clip
+	del ipp.txt
 	echo 按任意键换下一个端点或者用Ctrl+C退出
 	pause
 )
